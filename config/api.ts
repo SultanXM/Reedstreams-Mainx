bc5a00e480c43b40573a9c3ca65dc6f896c552cb
SultanXM <sultanreha583@gmail.com>
2026-02-23 05:33:14 +0500

    fuck

--- config/api.ts
+++ config/api.ts
@@ -1,6 +1,6 @@
 // Centralized API Configuration
 // Change this URL to switch to a different API endpoint
-export const API_BASE_URL = 'https://api.reedstreams.live'
+export const API_BASE_URL = 'reedstreams-edge-v1.fly.dev'
 export const API_V1_URL = `${API_BASE_URL}/api/v1`
 export const API_STREAMS_URL = `${API_V1_URL}/streams`
 
